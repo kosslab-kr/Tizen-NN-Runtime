fca28387f44791e07f6686207c917bfd91eeaf58
Sehyeon Jo <lazineer@gmail.com>
2018-10-09 02:57:22 -0700

    check webcam opened

--- webcam/src/cam.cpp
+++ webcam/src/cam.cpp
@@ -4,22 +4,28 @@
 int main()
 {
     std::cout << "OpenCV Version : " << CV_VERSION << std::endl;
-    cv::Mat img;
-    cv::namedWindow("EXAMPLE02", 1);
+//    cv::Mat img;
+//    cv::namedWindow("EXAMPLE02", 1);
     cv::VideoCapture cap;
  
-    cap.open(0); // 노트북 카메라는 cap.open(1) 또는 cap.open(-1)
+    cap.open(6); // 노트북 카메라는 cap.open(1) 또는 cap.open(-1)
                      // USB 카메라는 cap.open(0);
 
-    while (cap.isOpened())
-    {
-        cap >> img;
-        cv::imshow("EXAMPLE02", img);
-        if (cv::waitKey(30)==27)
-        {
-            break;
-        }
-    }
-    cv::destroyWindow("EXAMPLE02");
+if (cap.isOpened())
+    std::cout << "cap opened" << std::endl;
+else
+    std::cout << "cap not opened" << std::endl;
+
+//    while (cap.isOpened())
+//    {
+//        cap >> img;
+//        cv::imshow("EXAMPLE02", img);
+//        if (cv::waitKey(30)==27)
+//        {
+//            break;
+//        }
+//    }
+//    cv::destroyWindow("EXAMPLE02");
+
     return 0;
 }
